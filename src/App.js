5ebd1a36ecb726e716feb09db457acce98c066e8
SoftwareKyle <kylethesoftwareengineer@gmail.com>
2022-08-04 23:48:50 -0700

    fix build error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 //import { Routes, Route, Link, Navigate, useNavigate} from 'react-router-dom';
 //import ReactGA from 'react-ga';
-import Example from './Components/example';
+//import Example from './Components/example';
 
 function App() {
   return (
@@ -15,18 +15,11 @@ function App() {
         </p>
         <br></br>
         <p>Testing Google Analytics 4</p>
-        <a
-          className="App-link"
-          href="kyedilbeck.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        
           GA4 WebDevelopment with ReactJS
-        </a>
+        
       </header>
-      <body>
-        <Example/>
-      </body>
+      
     </div>
   );
 }
