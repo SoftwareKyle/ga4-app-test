38d9dc95b1a56160996a627515d47b530ef7ac49
SoftwareKyle <kylethesoftwareengineer@gmail.com>
2022-08-04 23:41:32 -0700

    adding GA4 tag collect data / analytics

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Example from './Components/example';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
